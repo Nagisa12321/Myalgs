616de487c1903dbd510e73692a079d3bd3c4df8e
Nagisa12321 <1216414009@qq.com>
2020-11-19 17:51:35 +0800

    初步配置maven本地仓库

--- src/test/java/com/JTChen/PracticalOperation/AccumulatorTest.java
+++ src/test/java/com/JTChen/PracticalOperation/AccumulatorTest.java
@@ -1,6 +1,6 @@
 package com.JTChen.PracticalOperation;
 
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4=.StdRandom;
 
 public class AccumulatorTest {
     public static void main(String[] args) {
